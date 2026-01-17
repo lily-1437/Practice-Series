62d5def4bc942155ca55f55faa64656050acbb35
lily-1437 <arpitanath2002@gmail.com>
2026-01-17 10:33:45 +0600

    New Changes

--- Day-6.py
+++ Day-6.py
@@ -14,17 +14,3 @@ def findDisappearedNumbers(nums):
 nums = [4,3,2,7,8,2,3,1]
 result = findDisappearedNumbers(nums)
 print(result)
-
-''' for finding duplicate numbers
-def findDuplicateNumbers(nums):
-    freq = [0] *(len(nums)+1)
-    result = []
-    for i in nums:
-        freq[i] +=1
-        if(freq[i]>1):
-            result.append(i)
-    return result
-
-nums = [1,3,2,7,8,2,3,1]
-result = findDuplicateNumbers(nums)
-print(result)'''
